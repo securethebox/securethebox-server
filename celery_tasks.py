8899b9a8df86a6f7cf9c02823f5a60af7004a047
charles 🔥 <charleschong@Charless-MacBook-Pro.local>
2019-12-07 11:46:48 -0800

    Removed airflow files

--- celery_tasks.py
+++
@@ -1,8 +0,0 @@
-from celery import Celery
-from celery.schedules import crontab
-
-app = Celery('celery_tasks', backend='db+postgresql://airflow:airflow@postgres:5432/airflow', broker='redis://redis:6379/0')
-
-@app.task
-def add(x, y):
-    return x + y
